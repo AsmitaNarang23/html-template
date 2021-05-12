4c4802b25bd539a77aa3cc61522bb907e819677e
AsmitaNarang23 <asmitanarang555@gmail.com>
2021-05-12 23:10:29 +0530

    callouts file added

---
+++ modules/callouts.php
@@ -0,0 +1,13 @@
+<div class="callout-wrapper">
+<div class="callout">
+<div class="container">
+   <div class="row">
+      <div class="col-md-7 col-md-5">
+        <div class="callout-box">
+           Our Services
+        </div>
+      </div>
+   </div>
+</div>
+</div>
+</div>
