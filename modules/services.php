8c80468ff422525238d0e2ee6d7a6bdcafd673e0
AsmitaNarang23 <asmitanarang555@gmail.com>
2021-05-12 22:45:43 +0530

    services php updates

--- modules/services.php
+++ modules/services.php
@@ -1,1 +1,44 @@
-xjscndcf jcdjvd
+<div class="services-wrapper">
+<div class="container">
+   <div class="row">
+      <div class="col-sm-12">
+        <span class="services-main-tile">
+           Our Services
+        </span>
+      </div>
+      <div class="col-md-4">
+        <span class="services-image"><img class="img-fluid" src="images/services/2.jpg" alt="">
+        </span>
+        <span class="services-sub-title">
+           <a href="#"> Lorem ipsum </a>
+        </span>
+        <span class="services-copy">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur
+        </span>
+      </div>
+
+      <div class="col-md-4">
+        <span class="services-image"><img class="img-fluid" src="images/services/2.jpg" alt="">
+        </span>
+        <span class="services-sub-title">
+          <a href="#"> Lorem ipsum</a>
+        </span>
+        <span class="services-copy">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur
+        </span>
+      </div>
+
+      <div class="col-md-4">
+        <span class="services-image"><img class="img-fluid" src="images/services/2.jpg" alt="">
+        </span>
+        <span class="services-sub-title">
+          <a href="#"> Lorem ipsum </a>
+        </span>
+        <span class="services-copy">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur
+        </span>
+      </div>
+
+    </div>
+</div>
+</div>
